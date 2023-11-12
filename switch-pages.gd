15b7bd827bb3fa283b75bcedb24d74b570f272d8
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-12 02:45:23 +0000

    Second session
    Implemented changing between pages (Game and options screen)
    Added text for upgrades and options screen
    Updated button element asset to fit more with style

--- switch-pages.gd
+++ switch-pages.gd
@@ -4,6 +4,9 @@ func _process(delta):
 	self.pressed.connect(self._switch)
 
 func _switch():
-	self.scale.y *= 0.7
-	await get_tree().create_timer(1).timeout
-	self.scale.y *= 1.428571429
+	for i in 4:
+		self.scale.y *= 0.7
+		await get_tree().create_timer(0.4).timeout
+	for i in 4:
+		self.scale.y *= 1.428571429
+		await get_tree().create_timer(0.4).timeout
